dce832f2274a00c667c888804a58c6dccc775a45
Claudio Freitas <claudiofreitas3@gmail.com>
2022-08-28 02:30:48 +0900

    configure tailwindcss

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  return <div>Hello world</div>;
+  return <div className="bg-red-500">Hello world</div>;
 };
 
 export default Home;
